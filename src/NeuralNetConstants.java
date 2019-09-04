1c9b865aeb00ad19e76b80839063ba7961e1246f
Bartoszek123 <44552510+Bartoszek123@users.noreply.github.com>
2019-09-04 21:45:27 +0200

    making classification

--- src/NeuralNetConstants.java
+++ src/NeuralNetConstants.java
@@ -6,6 +6,6 @@ public class NeuralNetConstants {
 
     public static final float LEARNING_RATE = 0.3f;
     public static final float MOMENTUM = 0.6f;
-    public static final float ITERATIONS = 1;
+    public static final float ITERATIONS = 10000;
 
 }
